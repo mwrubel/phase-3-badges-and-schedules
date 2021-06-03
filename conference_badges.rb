7b7ffdd98fd4eaa8c717ffff1ac4e3451e13f653
Mike <mikewrubel51@gmail.com>
2021-06-03 10:55:10 -0400

    commit tree 2a8ead1e521e693f3d03a68d792879694d10e9f1

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def badge_maker(name)
+    puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    attendees.each do |atendee|
+    puts badges.push("Hello, my name is #{atendee}.")
+    
+    end
+end
